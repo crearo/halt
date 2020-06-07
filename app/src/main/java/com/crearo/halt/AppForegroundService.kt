1087610e2ddfcedfec36e74e11977df75585e756
rish <bhardwaj.rish@gmail.com>
2020-06-07 15:57:12 +0200

    Add a foreground service
    Gonna test things out within this service

---
+++ app/src/main/java/com/crearo/halt/AppForegroundService.kt
@@ -0,0 +1,62 @@
+package com.crearo.halt
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.content.ContextCompat
+
+class AppForegroundService : Service() {
+
+    private val CHANNEL_ID = "AppForegroundService"
+
+    companion object {
+        fun startService(context: Context) {
+            val intent = Intent(context, AppForegroundService::class.java)
+            ContextCompat.startForegroundService(context, intent)
+        }
+
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, AppForegroundService::class.java)
+            context.stopService(stopIntent)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, 0
+        )
+        val notification: Notification =
+            Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(getText(R.string.notification_title))
+                .setContentText(getText(R.string.notification_message))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .setTicker(getText(R.string.ticker_text))
+                .build()
+        startForeground(1, notification)
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("AppForeground", "onCreate()")
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    private fun createNotificationChannel() {
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID, "Foreground Service Channel",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager!!.createNotificationChannel(serviceChannel)
+    }
+}
