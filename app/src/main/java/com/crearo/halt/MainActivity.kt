5e9b72a7470f55ad5729c11b794bcf72389b80f3
rish <bhardwaj.rish@gmail.com>
2020-06-08 23:35:27 +0200

    Enable/Disable DND programatically
    This is awesome. This way I can hide notifs for later. And this way I
    won't wake up with all these notifications and so I'd have a lower
    urge to waster time.

--- app/src/main/java/com/crearo/halt/MainActivity.kt
+++ app/src/main/java/com/crearo/halt/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.crearo.halt
 
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,14 +9,18 @@ import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
 
+
 class MainActivity : AppCompatActivity() {
 
     private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
+
+    private lateinit var notificationManager: NotificationManager;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppForegroundService.startService(this)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 
     override fun onResume() {
@@ -24,6 +30,18 @@ class MainActivity : AppCompatActivity() {
             intent.data = Uri.parse("package:$packageName")
             startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
         }
+
+        if (!notificationManager.isNotificationPolicyAccessGranted) {
+            val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+            startActivity(intent)
+        } else {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
