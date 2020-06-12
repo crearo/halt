c52b2b02abe407a5fe92683f66b4973496a9d1e3
rish <bhardwaj.rish@gmail.com>
2020-06-12 03:38:38 +0200

    Remove PhoneLockBroadcastReceiver cuz that's useless
    Sadly I gotta poll. But fuck RxJava is so fucking cool.

--- app/src/main/java/com/crearo/halt/AppForegroundService.kt
+++ app/src/main/java/com/crearo/halt/AppForegroundService.kt
@@ -3,8 +3,6 @@ package com.crearo.halt
 import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.*
-import android.content.IntentFilter
 import android.os.IBinder
 import android.os.PowerManager
 import androidx.core.content.ContextCompat
@@ -14,16 +12,12 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
-import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class AppForegroundService : Service() {
 
     private val CHANNEL_ID = "AppForegroundService"
-
-    @Inject
-    lateinit var phoneLockReceiver: PhoneLockBroadcastReceiver
 
     private val compositeDisposable = CompositeDisposable()
 
@@ -60,7 +54,6 @@ class AppForegroundService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        registerReceivers()
         compositeDisposable.add(
             Observable.interval(1, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
@@ -84,20 +77,7 @@ class AppForegroundService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceivers()
         compositeDisposable.clear()
-    }
-
-    private fun registerReceivers() {
-        val phoneLockIntentFilter = IntentFilter()
-        phoneLockIntentFilter.addAction(ACTION_USER_PRESENT)
-        phoneLockIntentFilter.addAction(ACTION_SCREEN_ON)
-        phoneLockIntentFilter.addAction(ACTION_SCREEN_OFF)
-        registerReceiver(phoneLockReceiver, phoneLockIntentFilter)
-    }
-
-    private fun unregisterReceivers() {
-        unregisterReceiver(phoneLockReceiver)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
