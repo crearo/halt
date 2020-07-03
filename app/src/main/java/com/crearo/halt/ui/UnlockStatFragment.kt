7fd7dfec29ad2d17df7db15edbed17d5be7a1039
rish <bhardwaj.rish@gmail.com>
2020-07-04 00:17:12 +0200

    Use the behaviour subject's value instead of a variable
    I think this makes more sense? Anyway this is debugging stuff, but yeah
    seems legit.
    
    Now onto investigating why duration in the future shows wrong

--- app/src/main/java/com/crearo/halt/ui/UnlockStatFragment.kt
+++ app/src/main/java/com/crearo/halt/ui/UnlockStatFragment.kt
@@ -12,7 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.BehaviorSubject
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -27,10 +27,7 @@ class UnlockStatFragment : Fragment() {
     private val compositeDisposable = CompositeDisposable()
     private var _binding: FragmentUnlockStatBinding? = null
     private val binding get() = _binding!!
-    private var startLocalDateTimeInUtc =
-        LocalDate.now(ZoneId.systemDefault()).atStartOfDay(ZoneId.systemDefault())
-    private val endLocalDateTime get() = startLocalDateTimeInUtc.plusDays(1)
-    private val localDateSubject = PublishSubject.create<ZonedDateTime>()
+    private val zonedDateTimeSubject = BehaviorSubject.create<ZonedDateTime>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,46 +37,49 @@ class UnlockStatFragment : Fragment() {
         _binding = FragmentUnlockStatBinding.inflate(layoutInflater, container, false)
 
         binding.btnMinusDay.setOnClickListener {
-            startLocalDateTimeInUtc = startLocalDateTimeInUtc.minusDays(1)
-            localDateSubject.onNext(startLocalDateTimeInUtc)
+            val startZonedDateTime = zonedDateTimeSubject.value!!
+            zonedDateTimeSubject.onNext(startZonedDateTime.minusDays(1))
         }
         binding.btnPlusDay.setOnClickListener {
-            startLocalDateTimeInUtc = startLocalDateTimeInUtc.plusDays(1)
-            localDateSubject.onNext(startLocalDateTimeInUtc)
+            val startZonedDateTime = zonedDateTimeSubject.value!!
+            zonedDateTimeSubject.onNext(startZonedDateTime.plusDays(1))
         }
 
-        compositeDisposable.add(localDateSubject.subscribe {
-            binding.tvDay.text = startLocalDateTimeInUtc.toString()
-            setUsageStatsTextViews()
+        compositeDisposable.add(zonedDateTimeSubject.subscribe {
+            binding.tvDay.text = it.toString()
+            setUsageStatsTextViews(it)
         })
 
-        localDateSubject.onNext(startLocalDateTimeInUtc)
+        zonedDateTimeSubject.onNext(
+            LocalDate.now(ZoneId.systemDefault()).atStartOfDay(ZoneId.systemDefault())
+        )
 
         return binding.root
     }
 
-    private fun setUsageStatsTextViews() {
+    private fun setUsageStatsTextViews(startZonedDateTime: ZonedDateTime) {
         compositeDisposable.add(repository
             .getTotalTimeUsed(
-                startLocalDateTimeInUtc.toInstant(),
-                endLocalDateTime.toInstant()
+                startZonedDateTime.toInstant(),
+                startZonedDateTime.plusDays(1).toInstant()
             )
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnSuccess {
-                binding.tvTotalTimeUsed.text = DateUtils.formatElapsedTime(it.seconds)
+                binding.tvTotalTimeUsed.text =
+                    "Total Time Used: ${DateUtils.formatElapsedTime(it.seconds)}"
             }
             .doOnError { binding.tvTotalTimeUsed.text = "Failed to get total time used" }
             .subscribe { t1, t2 -> }
         )
         compositeDisposable.add(
             repository
-                .getFirstUnlock(startLocalDateTimeInUtc.toLocalDate()/*fixme: now this is wrong. I should change the underlying call to be sure exactly what date I am looking for*/)
+                .getFirstUnlock(startZonedDateTime.toLocalDate()/*fixme: now this is wrong. I should change the underlying call to be sure exactly what date I am looking for*/)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnSuccess {
                     binding.tvFirstUnlock.text =
-                        it.unlockTime.atZone(ZoneId.systemDefault()).toString()
+                        "First Time Used: ${it.unlockTime.atZone(ZoneId.systemDefault())}"
                 }
                 .doOnError { binding.tvFirstUnlock.text = "Failed to get first time used" }
                 .subscribe { t1, t2 -> }
