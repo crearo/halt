e50d4443f50afda3605c46a58d5dcf6fe598e31f
rish <bhardwaj.rish@gmail.com>
2020-07-17 00:32:17 +0200

    Add some things with buttons but have a much better idea
    Single button. So the bottom won't have two, but one button. And just
    dismissing the bottom will put you in Focus mode by default say whaaaa
    this is awesome i love it

--- app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
+++ app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
@@ -4,20 +4,42 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.crearo.halt.databinding.ActivityChooseIntentBinding
+import com.crearo.halt.manager.FocusModeManager
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlin.random.Random
 
 
 @AndroidEntryPoint
 class ChooseIntentActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var focusModeManager: FocusModeManager
+
     private lateinit var binding: ActivityChooseIntentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChooseIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = ""
+
+        val randomButton = if (Random(10).nextBoolean()) binding.btnLeft else binding.btnRight
+        setupFocusButton(if (randomButton == binding.btnLeft) binding.btnLeft else binding.btnRight)
+        setupSocialButton(if (randomButton == binding.btnLeft) binding.btnRight else binding.btnLeft)
+    }
+
+    private fun setupSocialButton(button: Button) {
+        button.text = "Social"
+        button.setOnClickListener { focusModeManager.setFocusMode(false) }
+    }
+
+    private fun setupFocusButton(button: Button) {
+        button.text = "Focus"
+        button.setOnClickListener { focusModeManager.setFocusMode(true) }
     }
 
     override fun onAttachedToWindow() {
