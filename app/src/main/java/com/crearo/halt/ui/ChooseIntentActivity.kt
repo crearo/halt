cbc6077e0f3b2e75a7ff8fe700264f85fd424a40
rish <bhardwaj.rish@gmail.com>
2020-07-17 00:51:22 +0200

    Keep single button for social
    Closing the activity will set you in DND

--- app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
+++ app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.crearo.halt.databinding.ActivityChooseIntentBinding
 import com.crearo.halt.manager.FocusModeManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import kotlin.random.Random
 
 
 @AndroidEntryPoint
@@ -20,26 +18,26 @@ class ChooseIntentActivity : AppCompatActivity() {
     lateinit var focusModeManager: FocusModeManager
 
     private lateinit var binding: ActivityChooseIntentBinding
+    private var stayInSocialMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChooseIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = ""
-
-        val randomButton = if (Random(10).nextBoolean()) binding.btnLeft else binding.btnRight
-        setupFocusButton(if (randomButton == binding.btnLeft) binding.btnLeft else binding.btnRight)
-        setupSocialButton(if (randomButton == binding.btnLeft) binding.btnRight else binding.btnLeft)
+        binding.social.setOnClickListener {
+            stayInSocialMode = true
+            focusModeManager.setFocusMode(false)
+            finish()
+        }
     }
 
-    private fun setupSocialButton(button: Button) {
-        button.text = "Social"
-        button.setOnClickListener { focusModeManager.setFocusMode(false) }
-    }
-
-    private fun setupFocusButton(button: Button) {
-        button.text = "Focus"
-        button.setOnClickListener { focusModeManager.setFocusMode(true) }
+    override fun onPause() {
+        super.onPause()
+        if (!stayInSocialMode) {
+            focusModeManager.setFocusMode(true)
+        }
+        stayInSocialMode = false
     }
 
     override fun onAttachedToWindow() {
