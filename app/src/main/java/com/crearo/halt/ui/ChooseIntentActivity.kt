4dd9ea2317f0fb7ac347f92f457e15bccb43238c
rish <bhardwaj.rish@gmail.com>
2020-07-16 23:53:07 +0200

    Intent chooser now shows up at the bottom!
    Woot! This is awesome.

--- app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
+++ app/src/main/java/com/crearo/halt/ui/ChooseIntentActivity.kt
@@ -1,9 +1,13 @@
 package com.crearo.halt.ui
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.crearo.halt.databinding.ActivityChooseIntentBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class ChooseIntentActivity : AppCompatActivity() {
@@ -13,6 +17,15 @@ class ChooseIntentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityChooseIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        title = ""
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val view: View = window.decorView
+        val lp = view.layoutParams as WindowManager.LayoutParams
+        lp.gravity = Gravity.BOTTOM
+        windowManager.updateViewLayout(view, lp)
     }
 
 }
